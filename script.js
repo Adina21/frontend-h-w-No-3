ab7309584fdd10261ba04cd00c35ee060c66aa5b
Adina21 <adina.ulfanova@gmail.com>
2024-02-17 11:26:03 +0300

    h/w No3

---
+++ script.js
@@ -0,0 +1,259 @@
+const obj = {
+    name: 'Adina',
+    last_name: 'Vakhidova',
+    phone: '+996222081262',
+    marriage: false,
+    gender: 'female',
+    address:   {street: 'Bedelova',
+    number: 14,
+    country: 'KG',
+    region: 'osh',
+    city: 'osh'},
+    family: {
+        mother: {
+            name: 'Gulnara',
+            last_name: 'Ulfanova',
+            phone: '+996556724356',
+            marriage: false,
+            gender: 'female',
+            address:   {street: 'Bedelova',
+            number: 14,
+            country: 'KG',
+            region: 'osh',
+            city: 'osh'},
+            family:
+            {
+                son: {name: 'Rustam',
+                last_name: 'Ulfanov',
+                phone: '+996556724356',
+                marriage: True,
+                gender: 'male',
+                address:   {street: 'Bedelova',
+                number: 14,
+                country: 'KG',
+                region: 'osh',
+                city: 'osh'},
+                },
+                dauther1: {name: 'Sevilya',
+                last_name: 'Vakhidova',
+                phone: '+996556724356',
+                marriage: True,
+                gender: 'female',
+                address:   {street: 'Bedelova',
+                number: 14,
+                country: 'KG',
+                region: 'osh',
+                city: 'osh'},
+                },
+                dauther2: {name: 'Adina',
+                last_name: 'Vakhidova',
+                phone: '+996222081262',
+                marriage: false,
+                gender: 'female',
+                address:   {street: 'Bedelova',
+                number: 14,
+                country: 'KG',
+                region: 'osh',
+                city: 'osh'},}
+            },
+
+            friends: 
+            [
+                {name: 'Gulsun',
+                last_name: 'Borubaeva',
+                phone: '+996222232325',
+                marriage: false,
+                gender: 'female',
+                address:   {street: 'Moydunov',
+                number: 4,
+                country: 'KG',
+                region: 'osh',
+                city: 'osh'},
+               },
+
+               {name: 'Gatyfa',
+                last_name: 'Kuramshina',
+                phone: '+79888092192',
+                marriage: True,
+                gender: 'female',
+                address:   {street: 'Nikolskaya',
+                number: 5,
+                country: 'RU',
+                region: 'Bashkirya',
+                city: 'Ufa'},
+               }
+            ],
+            workplace: {
+                name: 'lawyer',
+                address: {
+                    street: 'Lenine',
+                    number: 305,
+                    country: 'KG',
+                    region: 'osh',
+                    city: 'osh'
+                },
+            },
+
+            skills: [
+                'Russia', 'law', 'constitution of the Kyrgyz Republic', 'Kyrgyz'
+            ]
+
+        },
+        brother: {
+            name: 'Rustam',
+            last_name: 'Ulfanov',
+            phone: '+996556724356',
+            marriage: True,
+            gender: 'male',
+            address:   {street: 'Bedelova',
+            number: 14,
+            country: 'KG',
+            region: 'osh',
+            city: 'osh'},
+        },
+        sister: {
+            name: 'Sevilya',
+            last_name: 'Vakhidova',
+            phone: '+996556724356',
+            marriage: True,
+            gender: 'female',
+            address:   {street: 'Bedelova',
+            number: 14,
+            country: 'KG',
+            region: 'osh',
+            city: 'osh'},
+            workplace: {
+                name: 'doctor',
+                address: {
+                    street: 'Kurmanjan Datka',
+                    number: 300,
+                    country: 'KG',
+                    region: 'osh',
+                    city: 'osh'
+                },
+            },
+            skills: [
+                'Russia', 'medicine', 'pratisyen', 'Kyrgyz'
+            ]
+        }
+    },
+    friends: [
+        {
+            name: 'Irina',
+            last_name: 'Kudayarova',
+            phone: '+7999899898',
+            marriage: True,
+            gender: 'female',
+            address:   {street: 'Marshala',
+            number: 10,
+            country: 'RU',
+            region: 'Bashkyrystan',
+            city: 'Ufa'},
+            family: {
+                husbend: {name: 'Artur',
+                last_name: 'Kudayarov',
+                phone: '+996556724356',
+                marriage: True,
+                gender: 'male',
+                address:   {street: 'Marshala',
+                number: 10,
+                country: 'RU',
+                region: 'Bashkyrystan',
+                city: 'Ufa'},     
+                workplace: {
+                    name: 'texsense',
+                    address: {
+                        street: 'Marshalaa',
+                        number: 10,
+                        country: 'RU',
+                        region: 'Bashkyrystan',
+                        city: 'Ufa'
+                    },
+                },
+                skills: [
+                    'Russia', 'YoutubeChanel', 'own bussiness', 'content'
+                ]
+
+                }
+            },
+            workplace: {
+                name: 'texsense',
+                address: {
+                    street: 'Marshalaa',
+                    number: 10,
+                    country: 'RU',
+                    region: 'Bashkyrystan',
+                    city: 'Ufa'
+                },
+            },
+            skills: [
+                'Russia', 'YoutubeChanel', 'own bussiness', 'content'
+            ]
+        },
+        {
+            name: 'Darya',
+            last_name: 'Shmid',
+            phone: '+79777089221',
+            marriage: True,
+            gender: 'female',
+            address:   {street: 'Pashkovskaya',
+            number: 8,
+            country: 'RU',
+            region: 'Krasnodar',
+            city: 'Krasnodar'},
+            family:{
+                husbend: {
+                    name: 'Andrey',
+                    last_name: 'Shmid',
+                    phone: '+79777989221',
+                    marriage: True,
+                    gender: 'male',
+                    address:  {street: 'Pashkovskaya',
+                    number: 8,
+                    country: 'RU',
+                    region: 'Krasnodar',
+                    city: 'Krasnodar'},
+                }
+            },
+            workplace: {
+                name: 'Magnit',
+                address: {
+                    street: 'Marshalaa',
+                    number: 10,
+                    country: 'RU',
+                    region: 'Bashkyrystan',
+                    city: 'Ufa'
+                },
+            },
+            skills: [
+                'Russia', 'programing', 'admin', 'maneger'
+            ]
+        },
+        {
+            name: 'Ayturgan',
+            last_name: 'Kanybekova',
+            phone: '+996222202895',
+            marriage: True,
+            gender: 'female',
+            address:   {street: 'Shaynazarova',
+            number: 7,
+            country: 'KG',
+            region: 'osh',
+            city: 'osh'},
+        }
+
+    ],
+    workplace: {
+        name: 'geeks',
+        address: {
+            street: 'abdimatalip amatova',
+            number: 2,
+            country: 'KG',
+            region: 'osh',
+            city: 'osh'
+        },
+    },
+    skills: [
+        'Russia', 'js', 'Turkish', 'py'
+    ]
+}
